ba01a2f33567ea70af5ebecdc9d5e9f02704fdf5
root <root@jherbage1.mylabserver.com>
2018-01-18 07:39:23 +0000

    base module ready

---
+++ functions/data.pp
@@ -0,0 +1,47 @@
+function nginx::data() {
+  $base_params = {
+    'nginx::$ensure'        => 'present',
+    'nginx::package_name'  => 'nginx',
+    'nginx::config_dir'    => '/etc/nginx',
+    'nginx::config_ensure' => 'file',
+    'nginx::config_mode'   => '0664',
+    'nginx::config_owner'  => 'root',
+    'nginx::config_group'  => 'root',
+    'nginx::config_confd'  => "${config_dir}/conf.d",
+    'nginx::config_log_dir' => '/var/log/nginx',
+    'nginx::config_pid_file' => 'run/nginx.pid',
+
+    'nginx::service_name' => 'nginx',
+    'nginx::service_ensure' => 'running',
+    'nginx::service_pattern' => 'nginx',
+    'nginx::service_enable' => true,
+    'nginx::service_hasstatus' => true,
+    'nginx::service_hasrestart' => true,
+   
+    'nginx::docroot' => '/usr/share/nginx',
+}
+
+    $config_vdir_enable = $facts['os']['family'] ? {
+      'Debian' => $config_dir,
+      default  => undef,
+    }
+ 
+    $config_process_user = $facts['os']['family'] ? {
+      'Debian' => 'www-data',
+      default  => 'nginx',
+    }
+
+    $vhost_dir = $facts['os']['family'] ? {
+      'Debian' => "${config_dir}/sites-enabled",
+      default  => $config_confd,
+    }
+
+    $extra_params = {
+      'nginx::config_vdir_enable' => $config_vdir_enable,
+      'nginx::config_process_user' => $config_process_user,
+      'nginx::vhost_dir' => $vhost_dir
+    }
+
+  $base_params + $extra_params
+
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,28 @@
 #
 # Copyright 2018 Your name here, unless otherwise noted.
 #
-class nginx {
-
-
+class nginx(
+  String $ensure,
+  String $package_name,
+  String $config_dir,
+  String $config_ensure,
+  String $config_mode,
+  String $config_owner,
+  String $config_group,
+  String $config_confd,
+  String $config_log_dir,
+  String $config_pid_file,
+  Optional[String] $config_vdir_enable,
+  String $config_process_user,
+  String $service_pattern,
+  String $service_ensure,
+  String $service_name,
+  Boolean $service_enable,
+  Boolean $service_hasstatus,
+  Boolean $service_hasrestart,
+  String $docroot,
+) {
+  class {'::nginx::install':}
+  -> class {'::nginx::config':}
+  ~> class {'::nginx::service':}
 }
